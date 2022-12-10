5454c9ab4ad7123ac2214a06e4c367b27f9e0961
Amir Mustafa <amirengg15@gmail.com>
2022-12-10 14:34:16 +0530

    fix: activityTrigger name in OE === activityTrigger function name

--- DurableFunctionsOrchestrator/index.ts
+++ DurableFunctionsOrchestrator/index.ts
@@ -4,9 +4,9 @@ const orchestrator = df.orchestrator(function* (context) {
   const outputs = [];
 
   // Replace "Hello" with the name of your Durable Activity Function.
-  outputs.push(yield context.df.callActivity("Hello", "Tokyo"));
-  outputs.push(yield context.df.callActivity("Hello", "Seattle"));
-  outputs.push(yield context.df.callActivity("Hello", "London"));
+  outputs.push(yield context.df.callActivity("HelloCity", "Tokyo"));
+  outputs.push(yield context.df.callActivity("HelloCity", "Seattle"));
+  outputs.push(yield context.df.callActivity("HelloCity", "London"));
 
   // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
   return outputs;
