139b80f0b376e1f67ad3c108d893c7228857b3d8
Amir Mustafa <amirengg15@gmail.com>
2022-12-13 07:51:54 +0530

    feat: initiate get repository by name - activity trigger

---
+++ GetRepositoryDetailsByName/index.ts
@@ -0,0 +1,9 @@
+﻿import { AzureFunction, Context } from "@azure/functions";
+
+const activityFunction: AzureFunction = async function (
+  context: Context
+): Promise<string> {
+  // WRITE CODE HERE
+};
+
+export default activityFunction;
