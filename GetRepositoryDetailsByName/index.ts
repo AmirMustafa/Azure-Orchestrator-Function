1670d3ae96a943f71f556649d3f972c171c8991e
Amir Mustafa <amirengg15@gmail.com>
2022-12-13 08:44:13 +0530

    feat: getting github repository userid - GetRepositoryDetailsByName activity trigger

--- GetRepositoryDetailsByName/index.ts
+++ GetRepositoryDetailsByName/index.ts
@@ -1,9 +1,21 @@
 ﻿import { AzureFunction, Context } from "@azure/functions";
+import { Octokit } from "@octokit/core";
 
 const activityFunction: AzureFunction = async function (
   context: Context
 ): Promise<string> {
-  // WRITE CODE HERE
+  const octokit = new Octokit();
+
+  const query = `${context.bindingData.repositoryName.toString()} in:name`;
+  const searhResult = await octokit.request("GET /search/repositories", {
+    q: query,
+  });
+
+  const exactMatch = searhResult.data.items.find(
+    (item) => item.name === context.bindingData.repositoryName.toString()
+  );
+
+  return exactMatch.owner.login;
 };
 
 export default activityFunction;
