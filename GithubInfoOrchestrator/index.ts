2767da8bc6e4dded1a6efecd821819ba88c1cad7
Amir Mustafa <amirengg15@gmail.com>
2022-12-17 13:24:44 +0530

    fix: merge conflicts

--- GithubInfoOrchestrator/index.ts
+++ GithubInfoOrchestrator/index.ts
@@ -1,18 +1,32 @@
 ﻿import * as df from "durable-functions";
 
 const orchestrator = df.orchestrator(function* (context) {
-  const userId: string = yield context.df.callActivity(
+  const firstRetryIntervalInMilliseconds: number = 1000;
+  const maxNumberOfAttempts: number = 3;
+  const maxRetryIntervalInSeconds: number = 1000;
+  const retryTimeoutInMilliseconds: number = 7000;
+
+  const retryConfig: df.RetryOptions = new df.RetryOptions(
+    firstRetryIntervalInMilliseconds,
+    maxNumberOfAttempts
+  );
+  retryConfig.maxRetryIntervalInMilliseconds = maxRetryIntervalInSeconds;
+  retryConfig.retryTimeoutInMilliseconds = retryTimeoutInMilliseconds;
+
+  const userId: string = yield context.df.callActivityWithRetry(
     "GetRepositoryDetailsByName",
+    retryConfig,
     context.bindingData.input
   );
 
   // Chaining response from first actitity trigger to next actitity
   context.bindingData.input.userId = userId;
 
-  context.log("OE ==>", userId, context.bindingData.input);
+  // context.log("OE ==>", userId, context.bindingData.input);
 
-  const userInfo = yield context.df.callActivity(
+  const userInfo = yield context.df.callActivityWithRetry(
     "GetUserDetailsById",
+    retryConfig,
     context.bindingData.input
   );
   return userInfo;
